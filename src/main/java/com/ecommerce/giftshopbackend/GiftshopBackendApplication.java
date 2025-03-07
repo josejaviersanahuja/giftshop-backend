98f97dbf8b5d2a97a02527547cc45c73297604cb
José Javier Sanahuja <josejaviersanahuja@gmail.com>
2025-03-07 11:38:18 +0000

    Spring Initializr OK

---
+++ src/main/java/com/ecommerce/giftshopbackend/GiftshopBackendApplication.java
@@ -0,0 +1,13 @@
+package com.ecommerce.giftshopbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GiftshopBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GiftshopBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ecommerce/giftshopbackend/GiftshopBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecommerce.giftshopbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GiftshopBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
